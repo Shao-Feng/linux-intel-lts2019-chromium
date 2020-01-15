d346122eba1921ecbaec568b2801ba0539536fdd
Colin Xu <colin.xu@intel.com>
2020-01-15 17:59:07 +0800

    drm/i915/gvt: Disable FBC and DMC firmware loading for IDV
    In IDV direct display, GVT handles DDB static allocation and host/vGPU
    display switch. When vGPU owns the display, host i915 isn't aware of
    actual display status, these display related dynamic power management
    may break vGPU operating display HW, and leads to issue like corruption
    and watermark underrun. So we disable these features in IDV.
    
    Signed-off-by: Colin Xu <colin.xu@intel.com>
    Acked-by: Hang Yuan <hang.yuan@intel.com>
    (cherry picked from commit e6bbc207ebb74a51be213b12d2ef722fee6db2e8)
    Signed-off-by: Colin Xu <colin.xu@intel.com>

--- drivers/gpu/drm/i915/intel_csr.c
+++ drivers/gpu/drm/i915/intel_csr.c
@@ -626,19 +626,28 @@ static void csr_load_work_fn(struct work_struct *work)
 	struct drm_i915_private *dev_priv;
 	struct intel_csr *csr;
 	const struct firmware *fw = NULL;
+	bool skip_dmc = false;
 
 	dev_priv = container_of(work, typeof(*dev_priv), csr.work);
 	csr = &dev_priv->csr;
 
-	/* Wait until root filesystem is loaded in case the firmware
-	 * is not built-in but in /lib/firmware */
-	WARN(_wait_for(system_state == SYSTEM_RUNNING,
-		       rootfs_timeout_ms * 1000, 10 * 1000, 100 * 1000),
-	     "Timing out after waiting %dms for SYSTEM_RUNNING",
-	     rootfs_timeout_ms);
+#if IS_ENABLED(CONFIG_DRM_I915_GVT)
+	if (dev_priv->gvt) {
+		skip_dmc = true;
+		DRM_WARN("Skip DMC firmware loading in IDV\n");
+	}
+#endif
 
-	request_firmware(&fw, dev_priv->csr.fw_path, &dev_priv->drm.pdev->dev);
-	parse_csr_fw(dev_priv, fw);
+	if (!skip_dmc) {
+		/* Wait until root filesystem is loaded in case the firmware
+	         * is not built-in but in /lib/firmware */
+        	WARN(_wait_for(system_state == SYSTEM_RUNNING,
+                	       rootfs_timeout_ms * 1000, 10 * 1000, 100 * 1000),
+	             "Timing out after waiting %dms for SYSTEM_RUNNING",
+        	     rootfs_timeout_ms);
+		request_firmware(&fw, dev_priv->csr.fw_path, &dev_priv->drm.pdev->dev);
+		parse_csr_fw(dev_priv, fw);
+	}
 
 	if (dev_priv->csr.dmc_payload) {
 		intel_csr_load_program(dev_priv);
